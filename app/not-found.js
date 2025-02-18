99fabe40de99d13ab8a23e4fdf9449dcdd57daa8
Bluetooxth <priyannxhuu@gmail.com>
2025-02-18 23:09:12 +0530

    ui issue

--- app/not-found.js
+++ app/not-found.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="flex justify-center items-center w-full h-screen font-[family-name:var(--font-secondary)]">
+    <div className="flex flex-col justify-center items-center w-full h-screen font-[family-name:var(--font-secondary)]">
       <p className="text-xl text-slate-100 font-medium">{`404: Page not found`}</p>
       <span className="text-md text-slate-200 font-normal">
         {`Sorry, the page you are looking for either does not exist. or in
